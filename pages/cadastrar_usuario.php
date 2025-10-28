61965c51d23e890865a43aa17f93961ccb89b9be
Guilherme Rocha <guilhermerochas0420@gmail.com>
2025-10-28 14:53:01 -0300

    feat: adicionado pagina de add, del e gerenciar usuarios

---
+++ pages/cadastrar_usuario.php
@@ -0,0 +1,140 @@
+<?php
+
+include('lib/conexao.php');
+include('lib/enviarArquivo.php');
+
+if (isset($_POST['enviar'])) {
+    $nome = $mysqli->real_escape_string($_POST['nome']);
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $creditos = $mysqli->real_escape_string($_POST['creditos']);
+    $senha = $mysqli->real_escape_string($_POST['senha']);
+    $rsenha = $mysqli->real_escape_string($_POST['rsenha']);
+
+    $erro = array();
+    if (empty($nome)) {
+        $erro[] = "Preencha o Nome.";
+    }
+
+    if (empty($email)) {
+        $erro[] = "Preencha o E-mail.";
+    }
+
+    if (empty($creditos)) {
+        $creditos = 0;
+    }
+
+    if (empty($senha)) {
+        $erro[] = "Preencha a Senha.";
+    }
+
+    if($rsenha != $senha) {
+        $erro[] = "As senhas não coincidem.";
+    }
+    if (count($erro) == 0) {
+        
+        $senha = password_hash($senha, PASSWORD_DEFAULT);
+        $mysqli->query("INSERT INTO usuarios (nome, email, senha, data_cadastro, creditos) VALUES('$nome', '$email', ('$senha'), NOW(), '$creditos')");
+        die("<script>location.href='index.php?p=gerenciar_usuarios';</script>");
+    }
+}
+
+?>
+
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-6">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Cadastrar Usuário</h4>
+                    <span>Preencha as informações e clique em Salvar para cadastrar um novo usuário.</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="index.php?p=gerenciar_usuarios">
+                            Gerenciar Usuários
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Cadastrar usuário</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+
+            <?php if (isset($erro) && count($erro) > 0) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php foreach ($erro as $e) {
+                        echo "$e<br>";
+                    } ?>
+                </div>
+            <?php } ?>
+
+            <div class="card">
+                <div class="card-header">
+                    <h5>Formulário de Cadastro</h5>
+                </div>
+                <div class="card-block">
+                    <form action="" method="POST">
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Nome</label>
+                                    <input type="text" name="nome" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">E-mail</label>
+                                    <input type="text" name="email" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Créditos</label>
+                                    <input type="text" name="creditos" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Senha</label>
+                                    <input type="text" name="senha" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label for="">Repetir senha</label>
+                                    <input type="text" name="rsenha" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <a href="index.php?p=gerenciar_usuarios" type="button" class="btn btn-primary btn-round"><i class="ti-arrow-left"></i> Voltar</a>
+                                <button type="submit" name="enviar" value="1" class="btn btn-success btn-round float-right"><i class="ti-save"></i> Salvar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
