1d7e22d592f71bdca3cf13408e26edae34d88e1f
Guilherme <guilhermerochas0420@gmail.com>
2025-10-30 00:04:24 -0300

    feat: adicionado logica de compra e acesso de cursos

--- pages/loja_cursos.php
+++ pages/loja_cursos.php
@@ -1,47 +1,97 @@
-    <!-- Page-header start -->
-    <div class="page-header card">
-        <div class="row align-items-end">
-            <div class="col-lg-8">
-                <div class="page-header-title">
-                    <div class="d-inline">
-                        <h4>Loja Gurunator</h4>
-                        <span>Adquira os cursos usando o seu crédito disponível!</span>
-                    </div>
+<?php
+
+include('lib/protect.php');
+protect(0);
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+    $erro = false;
+if(isset($_POST['adquirir'])) {
+
+    // verificar se o usuário possuí créditos para comprar o curso
+    $id_user = $_SESSION['usuario'];
+    $sql_query_creditos = $mysqli->query("SELECT creditos FROM usuarios WHERE id='$id_user'") or die($mysqli->error);
+    $usuario = $sql_query_creditos->fetch_assoc();
+
+    $creditos_do_usuario = $usuario['creditos'];
+
+    $id_curso = $_POST['adquirir'];
+    $sql_query_curso = $mysqli->query("SELECT preco FROM cursos WHERE id='$id_curso'") or die($mysqli->error);
+    $curso = $sql_query_curso->fetch_assoc();
+
+    $preco_do_curso = $curso['preco'];
+
+    if($preco_do_curso > $creditos_do_usuario) {
+        $erro = "Você não possui créditos suficientes para adquirir esse curso";
+    } else {
+        $mysqli->query("INSERT INTO relatorio (id_usuario, id_curso, valor, data_compra) VALUES ('$id_user', '$id_curso', '$preco_do_curso', NOW())") or die($mysqli->error);
+        $novo_credito = $creditos_do_usuario - $preco_do_curso;
+        $mysqli->query("UPDATE usuarios SET creditos='$novo_credito' WHERE id='$id_user'") or die($mysqli->error);
+        die("<script>location.href='index.php?p=meus_cursos';</script>");
+    }
+}
+
+$id_usuario = $_SESSION['usuario'];
+$cursos_query = $mysqli->query("SELECT * FROM cursos WHERE id NOT IN $(SELECT id_curso FROM relatorio WHERE id_usuario ='$id_usuario')") or die($mysqli->error);
+
+?>
+
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Loja Gurunator</h4>
+                    <span>Adquira os cursos usando o seu crédito disponível!</span>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="page-header-breadcrumb">
-                    <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="index.php">
-                                <i class="icofont icofont-home"></i>
-                            </a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="#!">Loja de Cursos</a>
-                        </li>
-                    </ul>
-                </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Loja de Cursos</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <!-- Page-header end -->
+</div>
+<!-- Page-header end -->
 
-    <div class="page-body">
-        <div class="row">
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <?php if ($erro != false) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $erro; ?>
+                </div>
+            <?php } ?>
+        </div>
+        <?php while ($curso = $cursos_query->fetch_assoc()) { ?>
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5>Projeto Nômade Milionário</h5>
+                        <h5><?php echo $curso['titulo']; ?></h5>
                     </div>
                     <div class="card-block">
-                        <img src="https://i.ytimg.com/vi/USCQo9v4HIg/maxresdefault.jpg" class="img-fluid mb-3">
+                        <img src="<?php echo $curso['imagem']; ?>" class="img-fluid mb-3">
                         <p>
-                            Na Gurunator, nós ensinamos diversos cursos interessantes e úteis para você, de forma
-                            acessível e completamente descomplicada!
-                            Aprenda na prática e com simplicidade.
+                            <?php echo $curso['descricao_curta']; ?>
                         </p>
-                        <button class="btn form-control btn-out-dashed btn-success btn-square">Adquirir por R$50,00</button>
+                        <form action="" method="POST">
+                            <button type="submit" name="adquirir" value="<?php echo $curso['id']; ?>" class="btn form-control btn-out-dashed btn-success btn-square">Adquirir por R$ <?php echo number_format($curso['preco'], 2, ',', '.'); ?></button>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        <?php } ?>
+    </div>
+</div>
--- pages/meus_cursos.php
+++ pages/meus_cursos.php
@@ -1,47 +1,61 @@
-    <!-- Page-header start -->
-    <div class="page-header card">
-        <div class="row align-items-end">
-            <div class="col-lg-8">
-                <div class="page-header-title">
-                    <div class="d-inline">
-                        <h4>Meus Cursos</h4>
-                        <span>Tenha acesso ao conteúdo dos seus cursos adquiridos!</span>
-                    </div>
+<?php
+
+include('lib/protect.php');
+protect(0);
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$id_usuario = $_SESSION['usuario'];
+$cursos_query = $mysqli->query("SELECT * FROM cursos WHERE id IN $(SELECT id_curso FROM relatorio WHERE id_usuario ='$id_usuario')") or die($mysqli->error);
+
+?>
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Meus Cursos</h4>
+                    <span>Tenha acesso ao conteúdo dos seus cursos adquiridos!</span>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="page-header-breadcrumb">
-                    <ul class="breadcrumb-title">
-                        <li class="breadcrumb-item">
-                            <a href="index.php">
-                                <i class="icofont icofont-home"></i>
-                            </a>
-                        </li>
-                        <li class="breadcrumb-item"><a href="#!">Meus Cursos</a>
-                        </li>
-                    </ul>
-                </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Meus Cursos</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
-    <!-- Page-header end -->
+</div>
+<!-- Page-header end -->
 
-    <div class="page-body">
-        <div class="row">
+<div class="page-body">
+    <div class="row">
+        <?php while ($curso = $cursos_query->fetch_assoc()) { ?>
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-header">
-                        <h5>Projeto Nômade Milionário</h5>
+                        <h5><?php echo $curso['titulo']; ?></h5>
                     </div>
                     <div class="card-block">
-                        <img src="https://i.ytimg.com/vi/USCQo9v4HIg/maxresdefault.jpg" class="img-fluid mb-3">
+                        <img src="<?php echo $curso['imagem']; ?>" class="img-fluid mb-3">
                         <p>
-                            Na Gurunator, nós ensinamos diversos cursos interessantes e úteis para você, de forma
-                            acessível e completamente descomplicada!
-                            Aprenda na prática e com simplicidade.
+                            <?php echo $curso['descricao_curta']; ?>
                         </p>
-                        <button class="btn form-control btn-out-dashed btn-primary btn-square">Assistir</button>
+                            <button class="btn form-control btn-out-dashed btn-success btn-square">Acessar</button>
                     </div>
                 </div>
             </div>
-        </div>
+        <?php } ?>
+    </div>
+</div>
